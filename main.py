97514ddf2bc5073aeacb2ad8f9a0ba89dbb1905d
Yossi Kovan <yossikovan@gmail.com>
2025-10-28 11:38:38 +0200

    added functions to the main and to the board file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from memory_game.board import create_hidden_matrix
+from memory_game.board import create_hidden_matrix, shuffle_matrix
 
 if __name__ == "__main__":
-   print(create_hidden_matrix(20))
+   matrix = create_hidden_matrix(20)
+   matrix = shuffle_matrix(matrix, 20)
+   print(matrix)
--- memory_game/board.py
+++ memory_game/board.py
@@ -8,26 +8,39 @@ def get_random_point(size):
 
 def shuffle_matrix(matrix, size):
 
-    max_shuffle = 10000
+    max_shuffle = 1000
 
     while max_shuffle > 0:
+
         row1, col1 = get_random_point(size)
         row2, col2 = get_random_point(size)
 
-        if row1 == row1 or col1 == col2:
-           continue
+        if row1 == row2 and col1 == col2:
+            continue
 
         matrix[row1][col1], matrix[row2][col2] = matrix[row2][col2], matrix[row1][col1]
+        max_shuffle -= 1
+
+    return matrix
 
 
 def create_hidden_matrix(size):
 
     #this will return a list of all the numbers
-    all_characters = [i for i in range(size**2 // 2)]
+    all_characters = [i for i in range(size**2 // 2)] * 2
 
+    matrix = []
 
+    for i in range(size):
+        arr = []
+        for j in range(size):
+            arr.append(all_characters[i*size + j])
+        matrix.append(arr)
+
+    return matrix
 
 
 def create_revealed_matrix(size):
+    """this will create the revealed matrix"""
     return [['o' for col in range(size)] for row in range(size)]
 
